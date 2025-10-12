14a98f670d3bbafb29523e49ace03d735f148a2e
Nathan Dias <nathandiasc@outlook.com>
2025-10-12 16:22:11 -0300

    fix: cancel/confirm buttons translation hook

--- src/components/buttons/CancelButton.tsx
+++ src/components/buttons/CancelButton.tsx
@@ -1,16 +1,18 @@
 import { ButtonStyles } from "@/components/buttons/ButtonStyles"
 import { FontStyles } from "@/components/styles/FontStyles"
 import { useTheme } from "@/context/ThemeContext"
+import { useTranslation } from "react-i18next"
 import { Text, TouchableOpacity, type TouchableOpacityProps } from "react-native"
 
 export default function CancelButton({...rest}: TouchableOpacityProps) {
 
-    const theme = useTheme()
-    const buttonStyles = ButtonStyles(theme.theme)
+    const {theme, preference, setPreference} = useTheme()
+    const {t} = useTranslation()
+    const buttonStyles = ButtonStyles(theme)
 
     return(
         <TouchableOpacity style={buttonStyles.cancelButton} onPress={rest.onPress}>
-            <Text style={[FontStyles.title2, {color: "#F5F5F5"}]}> Cancel </Text>
+            <Text style={[FontStyles.title2, {color: theme.menuItem.textOverTint}]}>{t("buttons.cancel")}</Text>
         </TouchableOpacity>
     )
 }
--- src/components/buttons/ConfirmButton.tsx
+++ src/components/buttons/ConfirmButton.tsx
@@ -1,5 +1,6 @@
 import { FontStyles } from "@/components/styles/FontStyles"
 import { useTheme } from "@/context/ThemeContext"
+import { useTranslation } from "react-i18next"
 import { Text, TouchableOpacity, type TouchableOpacityProps } from "react-native"
 
 
@@ -7,10 +8,11 @@ import { Text, TouchableOpacity, type TouchableOpacityProps } from "react-native
 export default function ConfirmButton({...rest}: TouchableOpacityProps) {
 
     const {theme, preference, setPreference} = useTheme()
+    const {t} = useTranslation()
 
     return(
         <TouchableOpacity style={rest.style} onPress={rest.onPress} disabled={rest.disabled}>
-            <Text style={[FontStyles.title2, {color: rest.disabled ? theme.menuItem.text : theme.menuItem.textOverTint}]}> Save </Text>
+            <Text style={[FontStyles.title2, {color: rest.disabled ? theme.menuItem.text : theme.menuItem.textOverTint}]}>{t("buttons.save")}</Text>
         </TouchableOpacity>
     )
 }
