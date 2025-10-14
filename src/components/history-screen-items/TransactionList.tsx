7b9121c647c79b1b8be6010834b11eeb77d3e5ef
Nathan Dias <nathandiasc@outlook.com>
2025-10-14 19:26:53 -0300

    feat: TransactionList

--- src/components/history-screen-items/TransactionList.tsx
+++ src/components/history-screen-items/TransactionList.tsx
@@ -1,15 +1,68 @@
-import { Transaction } from "@/database/useTransactionDatabase";
-import { useState } from "react";
+import { Transaction, TransactionFilterOptions, useTransactionDatabase } from "@/database/useTransactionDatabase";
+import { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
+import TransactionListItem from "./TransactionListItem";
 
 type TransactionListProps = {
+    filters: TransactionFilterOptions,
     onItemPress: (item: Transaction) => void,
 }
 
-export default function TransactionList({onItemPress}: TransactionListProps) {
+const PAGE_SIZE = 15
+
+export default function TransactionList({filters={type: "all", category: undefined}, onItemPress}: TransactionListProps) {
+    const {getPaginatedFilteredTransactions} = useTransactionDatabase()
+
     const [data, setData] = useState<Transaction[]>([])
     const [page, setPage] = useState(0)
     const [loading, setLoading] = useState(false)
     const [hasMore, setHasMore] = useState(true)
 
-    
+    const loadData = useCallback(async (isInitialLoad = false) => {
+        if(loading || (!isInitialLoad && !hasMore)) return
+
+        setLoading(true)
+
+        const currentPage = isInitialLoad ? 0 : page
+
+        try {
+            const newTransactions = await getPaginatedFilteredTransactions(page, PAGE_SIZE, filters)
+
+            if(isInitialLoad) {
+                setData(newTransactions)
+            } else {
+                setData(prev => [...prev, ...newTransactions])
+            }
+
+            setPage(currentPage + 1)
+            setHasMore(newTransactions.length === PAGE_SIZE)
+
+        } catch (error) {
+            console.error("Failed to load transactions", error);
+        } finally {
+            setLoading(false);
+        }
+    }, [loading, hasMore, page, filters, getPaginatedFilteredTransactions])
+
+    useEffect(() => {
+        // When filters change, reset the list and load from page 1
+        loadData(true);
+    }, [filters]);
+
+    const renderItem = ({item}: {item: Transaction}) => (
+        <TransactionListItem {...item} />
+    )
+
+    return(
+        <FlatList
+            data={data}
+            keyExtractor={(item: Transaction) => item.id.toString()}
+            renderItem={renderItem}
+            onEndReached={() => loadData()} // Load more on scroll
+            onEndReachedThreshold={0.5}
+            ListFooterComponent={loading ? <ActivityIndicator style={{ margin: 20 }}/> : null}
+        />
+    )
+
+
 }
